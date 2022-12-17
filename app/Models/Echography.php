a1ab1ffb33d70212accfe4980e0630d8c82fb238
Socheatha Tey <teysocheatha@gmail.com>
2022-12-17 09:17:41 +0700

    --

--- app/Models/Echography.php
+++ app/Models/Echography.php
@@ -17,6 +17,6 @@ class Echography extends BaseModel
     }
 
     public function getfilterAttrAttribute () {
-        return array_except(filter_unit_attr(unserialize($this->attribute) ?: []), ['patient_id', 'gender_id', 'age', 'doctor_id', 'status', 'amount', 'price', 'payment_type', 'address_id', 'pt_province_id', 'pt_district_id', 'pt_commune_id', 'pt_village_id', 'name_kh', 'name_en', 'index']);
+        return array_except(filter_unit_attr(unserialize($this->attribute) ?: []), ['patient_id', 'gender_id', 'age', 'doctor_id', 'status', 'amount', 'price', 'payment_type', 'address_id', 'pt_province_id', 'pt_district_id', 'pt_commune_id', 'pt_village_id', 'name_kh', 'name_en', 'index', 'requested_by']);
     }
 }
