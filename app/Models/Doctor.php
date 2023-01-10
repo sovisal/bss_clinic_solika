7202e688c90f66b38da8e042749fac12cfdcad23
SokunSovisal <sovisalsokun@gmail.com>
2023-01-10 21:19:50 +0700

    Patient & Doctor Link Attribute

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -13,7 +13,7 @@ class Doctor extends BaseModel
     public function getLinkAttribute()
     {
         if ($this->status > 0 && can('UpdateDoctor')) { // will check permission
-            return d_link(d_obj($this, ['name_en', 'name_kh']), route('setting.doctor.edit', [d_obj($this, 'doctor', 'id'), 'back' => url()->current()]));
+            return d_link(d_obj($this, ['name_en', 'name_kh']), route('setting.doctor.edit', [d_obj($this, 'id'), 'back' => url()->current()]));
         } else {
             return d_obj($this, ['name_en', 'name_kh']);
         }
